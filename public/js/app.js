dc64259bba61d72c7a6d7034fe8fc8f55d542cc1
bernat-ferrer <bernatferrerm@gmail.com>
2020-12-21 18:46:25 +0100

    icon

--- public/js/app.js
+++ public/js/app.js
@@ -21338,8 +21338,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/luis/Desktop/master fullstack/laravel/social-network-with-laravel/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/luis/Desktop/master fullstack/laravel/social-network-with-laravel/resources/css/app.css */"./resources/css/app.css");
+__webpack_require__(/*! /home/berni/Assembler/projects/social-network-with-laravel/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/berni/Assembler/projects/social-network-with-laravel/resources/css/app.css */"./resources/css/app.css");
 
 
 /***/ })
