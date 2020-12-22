4d691094cd8cbb0666094e8b5bc2f2d3b7de2101
bernat-ferrer <bernatferrerm@gmail.com>
2020-12-22 21:25:33 +0100

    welcome page styling

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,33 +14,33 @@
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 </head>
 
-<body class="font-mono bg-gray-400">
+<body class="font-mono bg-white">
     <!-- Container -->
-    <div class=" container mx-auto">
-        <div class=" flex justify-center px-6 my-12">
+    <div class=" h-800 container mx-auto my-auto">
+        <div class=" container-welcome h-500 flex justify-center px-6 my-12">
             <!-- Row -->
             <div class="w-full xl:w-3/4 lg:w-11/12 flex">
                 <!-- Col -->
                 <div class="w-full h-auto bg-white hidden lg:block lg:w-1/2 bg-cover rounded-l-lg"
                     style="background-image: url(images/original.gif)"></div>
                 <!-- Col -->
-                <div class="w-full lg:w-1/2 bg-black p-5 rounded-lg lg:rounded-l-none">
+                <div class="login-container w-full lg:w-1/2  p-5 rounded-lg lg:rounded-l-none">
                     <img class="mx-auto my-20" src="/images/icon2.png">
 
                     @if (Route::has('login'))
                         <div class="welcome-buttons-wrapper container mx-auto">
                             @auth
                                 <a id="welcome-dashboard" href="{{ url('/dashboard') }}"
-                                    class="border border-indigo-500 bg-indigo-500 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-indigo-600 focus:outline-none focus:shadow-outline">
+                                    class="mx-auto border border-blue-400 bg-blue-400 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-blue-600 focus:outline-none focus:shadow-outline">
                                     Dashboard
                                 </a>
                             @else
                                 <a href="{{ route('login') }}"
-                                    class="mx-auto border border-indigo-500 bg-indigo-500 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-indigo-600 focus:outline-none focus:shadow-outline">
+                                    class="mx-auto border border-blue-400 bg-blue-400 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-blue-600 focus:outline-none focus:shadow-outline">
                                     Login</a>
                                 @if (Route::has('register'))
                                     <a href="{{ route('register') }}"
-                                        class="mx-auto border border-indigo-500 bg-indigo-500 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-indigo-600 focus:outline-none focus:shadow-outline">
+                                        class="mx-auto border border-blue-400 bg-blue-400 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-blue-600 focus:outline-none focus:shadow-outline">
                                         Register</a>
                                 @endif
                             @endauth
